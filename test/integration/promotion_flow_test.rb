fb54074d7e79723b430bc29700355571cdb569b0
Marcos Antonio <marcosantoniotaina@gmail.com>
2021-03-29 14:57:54 -0300

    teste de integração para nao criar promoção sem estar logado

--- test/integration/promotion_flow_test.rb
+++ test/integration/promotion_flow_test.rb
@@ -13,8 +13,24 @@ class PromotionFlowTest < ActionDispatch::IntegrationTest
   	  	expiration_date: '22/12/2033'
   	  }
   	}
+
   	assert_redirected_to promotion_path(Promotion.last)
   	follow_redirect!  
   	assert_select 'p', 'Natal'
+  end
+
+  test 'can not create a promotion without login' do
+  	post '/promotions', params: {
+  	  promotion: {
+  	  	name: 'Natal',
+  	  	description: 'Promoção de Natal',
+  	  	code: 'NATAL10',
+  	  	discount_rate: 15,
+  	  	coupon_quantity: 5,
+  	  	expiration_date: '22/12/2033'
+  	  }
+  	}
+
+  	assert_redirected_to new_user_session_path
   end
 end
